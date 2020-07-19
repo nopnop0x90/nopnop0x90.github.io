f9ede2178fb8e3d391b149d6515107731b6ef03f
nopnop0x90 <61439621+nopnop0x90@users.noreply.github.com>
2020-07-18 18:45:03 -0700

    Update s.js

--- s.js
+++ s.js
@@ -1,4 +1,13 @@
 $(()=>
 {
-  $('#sec1').text('Section 1');
+  let i = 0;
+  const NUM_SECTIONS = 10;
+  let h = document.createElement('h2');
+  while (i < NUM_SECTIONS)
+  {
+    i++;
+    h.innerText = 'section ' + i;
+    document.body.append(h);
+    h = document.createElement('h2');
+  }
 });
